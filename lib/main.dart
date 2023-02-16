8c5a96ab12da9c96d7b4e7bc2d6115df6be5887e
3bdallahaboasab <abdallahaboasab@gmail.com>
2023-02-16 14:20:57 +0300

    16-2-2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,107 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter/services.dart';
 
-import 'maps.dart';
-
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final title = 'My title';
+
     return MaterialApp(
-      supportedLocales: const [Locale("ar", ""), Locale("en", "")],
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      localeResolutionCallback: (currentLang, supportLang) {
-        if (currentLang != null) {
-          for (Locale locale in supportLang) {
-            if (locale.languageCode == currentLang.languageCode) {
-              return currentLang;
-            }
-          }
-        }
-        return supportLang.first;
-      },
+      home: Scaffold(
+          body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            // title: Text("Dddd"),
+
+            scrolledUnderElevation: 0,
+            snap: false,
+
+            pinned: true,
+            floating: false,
+            // automaticallyImplyLeading: false,
+            flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                title: Text("$title",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ) //TextStyle
+                    ), //Text
+                background: Image.network(
+                  "https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg",
+                  fit: BoxFit.cover,
+                ) //Images.network
+                ), //FlexibleSpaceBar
+            expandedHeight: 230,
+            backgroundColor: Colors.orangeAccent[400],
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: 'Menu',
+              onPressed: () {},
+            ), //IconButton
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.comment),
+                tooltip: 'Comment Icon',
+                onPressed: () {},
+              ), //IconButton
+              IconButton(
+                icon: Icon(Icons.settings),
+                tooltip: 'Setting Icon',
+                onPressed: () {},
+              ), //IconButton
+            ],
+            systemOverlayStyle: SystemUiOverlayStyle.light, //<Widget>[]
+          ),
+          SliverGrid(
+              delegate: SliverChildBuilderDelegate((context, index) {
+                return listItem(Colors.primaries[Random().nextInt(5)],
+                    "Sliver Grid item:\n$index");
+              }),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 15,
+                  childAspectRatio: 15)), //SliverAppBar
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => ListTile(
+                tileColor: (index % 2 == 1) ? Colors.white : Colors.orange[50],
+                title: Center(
+                  child: Text('$index',
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 50,
+                          color: Colors.orangeAccent[400]) //TextStyle
+                      ), //Text
+                ), //Center
+              ), //ListTile
+              childCount: 51,
+            ), //SliverChildBuildDelegate
+          ),
+        ], //<Widget>[]
+      ) //CustonScrollView
+          ), //Scaffold
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      home: MapSample(),
-    );
+    ); //MaterialApp
   }
+
+  Widget listItem(Color color, String title) => Container(
+        height: 100.0,
+        color: color,
+        child: Center(
+          child: Text(
+            "$title",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 10.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      );
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus_macos
 import location
 import package_info_plus
+import path_provider_foundation
 import shared_preferences_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
